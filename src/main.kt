e6149bc7d0bd9560a7cbfe01de4ea79bc7cc722f
ghrasko <gabor@hrasko.com>
2019-12-03 17:47:48 +0000

    Variable examples are starting.

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,10 @@
-fun main() {
-    println("Hello World!")
-    println("A second line of code")
+val myName = "Gábor"
+
+fun main(args: Array<String>) {
+    val i = 3.toDouble()
+
+    println("Hello ${args[0]}")
+    println("The first argument is " + args[0])
+    println("i is a simple number: $i")
+    println("My name is $myName")
 }
