a991efbb8615d16ff37de07df882db11492e8ee2
ghrasko <gabor@hrasko.com>
2019-12-03 18:24:42 +0000

    Simple operations

--- src/main.kt
+++ src/main.kt
@@ -7,4 +7,21 @@ fun main(args: Array<String>) {
     println("The first argument is " + args[0])
     println("i is a simple number: $i")
     println("My name is $myName")
+
+    println("The type of i is ${i::class.simpleName}")
+    println("The qualified name of i is ${i::class.qualifiedName}")
+
+    var num1 = 12
+    val num2 = 10
+
+    val match:Boolean = (num1 == num2)
+    println("Match = $match")
+
+//    val match2:Boolean = num1.equals(num2)
+//    println("Match2 = $match2")
+
+    println("Comparison result = ${num1.compareTo(num2)}")
+
+    num1++
+    println("Increased num1 is $num1")
 }
