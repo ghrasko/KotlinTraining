fc1c9e27b5eb3ae33a8159a1cb0500b7ddc4cc39
ghrasko <gabor@hrasko.com>
2019-12-04 10:45:15 +0000

    Simple calculator test code

--- src/Constants.kt
+++
@@ -1,5 +0,0 @@
-class Constants {
-    companion object {
-        const val RED = "red"
-    }
-}
--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,35 @@
-import Constants.Companion.RED
+import kotlin.NumberFormatException
 
 fun main(args: Array<String>) {
-    println("The color is $RED")
+    try {
+        println("Enter 1st number: ")
+        val num1 : Double = readLine()!!.toDouble()
+        println("Select operator: + - * /")
+        val op : String? = readLine()
+        println("Enter 2nd number: ")
+        val num2 : Double = readLine()!!.toDouble()
+
+        val res : Double = when (op) {
+            "+" -> num1 + num2
+            "-" -> num1 - num2
+            "*" -> num1 * num2
+            "/" -> num1 / num2      // Will not cause "Division by Zero" exception with doubles. Results "Infinity"
+            else -> throw Exception("Invalid operator")
+        }
+
+        println("$num1 $op $num2 = $res")
+    }
+    catch(e: KotlinNullPointerException) {                  // One can imitate this by typing Ctrl-D
+        println("ERROR: Input is missing or unexpectedly terminated.")
+    }
+    catch(e: NumberFormatException) {
+        println("ERROR: Not a valid number format: ${e.message}")
+    }
+    catch(e: ArithmeticException) {                         // Will never happen with double variables
+        println("ERROR: Division by zero.")
+    }
+    catch(e: Exception) {
+        println("ERROR: ${e.message}")
+    }
 }
 
